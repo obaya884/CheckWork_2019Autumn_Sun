6665bcfaa20ef3a22469f00d0c67f458a4c509ae
otakumi <otakumi@m.cs.osakafu-u.ac.jp>
2019-12-01 01:30:53 +0900

    AR表示、カメラロール保存、BGM再生

--- CheckWork_2019Autumn_Sun/ViewController.swift
+++ CheckWork_2019Autumn_Sun/ViewController.swift
@@ -9,25 +9,72 @@
 import UIKit
 import SceneKit
 import ARKit
+import AVFoundation
 
 class ViewController: UIViewController, ARSCNViewDelegate {
 
     @IBOutlet var sceneView: ARSCNView!
+    var index: Int = 0
+    let pokemonNameArray: [String] = ["Blastoise", "Bulbasaur", "Charmander", "Cubone", "pm0132_00", "Eevee", "Flareon", "Gardevoir", "Glaceon", "Groudon", "Growlithe", "Jigglypuff", "Jolteon", "Leafeon", "Lucario", "Lugia", "Magikarp", "Meowth", "Mew", "pmxxx1_00_fi", "Oddish", "Pichu", "pikachu", "Psyduck", "Reshiram", "Torchic", "Typhlosion", "Umbreon", "Vaporeon", "Vulpix", "pm0768_00_fi", "Zorua"]
+    
+    var bgmPlayer: AVAudioPlayer!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // Set the view's delegate
         sceneView.delegate = self
         
-        // Show statistics such as fps and timing information
-        sceneView.showsStatistics = true
+        let bgmPath = URL(fileURLWithPath: Bundle.main.path(forResource: "bgm", ofType: "mp3")!)
+        bgmPlayer = try? AVAudioPlayer(contentsOf: bgmPath)
+        bgmPlayer?.play()
         
+        let string = "pikachu"
         // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+        let scene = SCNScene(named: "art.scnassets/\(string).scn")!
+        scene.rootNode.scale = SCNVector3(0.001, 0.001, 0.001)
+        sceneView.scene.rootNode.addChildNode(scene.rootNode)
         
-        // Set the scene to the view
-        sceneView.scene = scene
+    }
+    
+    @IBAction func next() {
+        sceneView.scene.rootNode.enumerateChildNodes{ (node, step) in
+            node.removeFromParentNode()
+        }
+        
+        if index != pokemonNameArray.count - 1 {
+            index += 1
+        }
+        else {
+            index = 0
+        }
+        
+        let scene = SCNScene(named: "art.scnassets/\(pokemonNameArray[index]).scn")!
+        scene.rootNode.scale = SCNVector3(0.001, 0.001, 0.001)
+        sceneView.scene.rootNode.addChildNode(scene.rootNode)
+
+    }
+    
+    @IBAction func back() {
+        sceneView.scene.rootNode.enumerateChildNodes{ (node, step) in
+            node.removeFromParentNode()
+        }
+        
+        if index == 0 {
+            index = pokemonNameArray.count - 1
+        }
+        else {
+            index -= 1
+        }
+        
+        let scene = SCNScene(named: "art.scnassets/\(pokemonNameArray[index]).scn")!
+        scene.rootNode.scale = SCNVector3(0.001, 0.001, 0.001)
+        sceneView.scene.rootNode.addChildNode(scene.rootNode)
+
+    }
+    
+    @IBAction func saveSnapShot() {
+        AudioServicesPlaySystemSound(1108)
+        let image: UIImage = sceneView.snapshot()
+        UIImageWriteToSavedPhotosAlbum(image, nil, nil, nil)
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -45,6 +92,8 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         // Pause the view's session
         sceneView.session.pause()
+        
+        bgmPlayer.stop()
     }
 
     // MARK: - ARSCNViewDelegate
